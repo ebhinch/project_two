df3798649f51488a53c0c73c4f9839a96af496ae
ebhinch <erica.hinchman@gmail.com>
2017-09-28 15:42:46 -0400

    Successfully built out index view and route. Thus far, the links don't point anywhere because views have not been built out. I also connected index route in app.js

--- routes/neighborhoodController.js
+++ routes/neighborhoodController.js
@@ -6,4 +6,9 @@ router.get('/neighborhood', function(request, response) {
   response.render("I work.");
 });
 
+
+router.get("/neighborhood", (request, response) => {
+  response.render
+})
+
 module.exports = router;
